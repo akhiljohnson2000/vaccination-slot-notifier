ab0065a2ec8d1ae0641cd7af6b1f89464c0123e3
Akhil Johnson <akhiljohnson2000@gmail.com>
2021-06-04 21:18:26 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,11 @@ import { DataService } from './app.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   title = "qwefqwef";
   spinner = false;
-  now: number; 
+  now: number;
   slotFinderStatus: boolean = true;
   setIntervalForApiCall;
   vaccineSlotsArray: Array<object> = [];
@@ -25,18 +25,19 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
     this.formName = this.fb.group({
-       pincode: ['',[
+      pincode: ['', [
         Validators.required,
         Validators.minLength(6),
         Validators.maxLength(6),
         Validators.pattern('^[0-9]*$')]]
     });
- }
+  }
 
   formSubmit(formData) {
     console.log(formData)
     clearInterval(this.setIntervalForApiCall)
     if (formData.valid) {
+      this.vaccineSlotsArray = []
       this.slotFinderStatusChange(formData);
       this.setIntervalForApiCall = setInterval(() => {
         this.slotFinderStatusChange(formData)
@@ -60,12 +61,33 @@ export class AppComponent implements OnInit{
 
     this.dataService.sendGetRequest(apiparam).subscribe((data: any[]) => {
       this.spinner = false;
-      data['centers'][0].time = this.now;
+      data['centers'].forEach(center => {
+        center.time = this.now;
+        center.max45CountD1 = 0;
+        center.max45CountD2 = 0;
+        center.max18CountD1 = 0;
+        center.max18CountD2 = 0;
+
+        center.sessions.forEach(date => {
+          if (date.min_age_limit == 45){
+            center.max45CountD1 = center.max45CountD1 + date.available_capacity_dose1;
+            center.max45CountD2 = center.max45CountD2 + date.available_capacity_dose2;
+          }
+          if (date.min_age_limit == 18){
+            center.max18CountD1 = center.max18CountD1 + date.available_capacity_dose1;
+            center.max18CountD2 = center.max18CountD2 + date.available_capacity_dose2;
+          }
+        });
+      });
+
+
+
       this.vaccineSlotsArray.push(data);
 
       // data['centers'][0].sessions[0].available_capacity = 1
 
-      if (data['centers'][0].sessions[0].available_capacity > 0 || data['centers'][0].sessions[0].available_capacity > 0) {
+      if (data['centers'][0].max18CountD1 > 0 ||
+      data['centers'][0].max45CountD1 > 0 ) {
         var audio = new Audio();
         audio.src = "https://d6cp9b00-a.akamaihd.net/downloads/ringtones/files/mp3/silent-beep-1859.mp3";
         audio.load();
