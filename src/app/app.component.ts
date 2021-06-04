6b6ac8eeb6425392c787aa6f7c6859c6cb6668c0
Akhil Johnson <akhiljohnson2000@gmail.com>
2021-06-04 14:43:56 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { DataService } from './app.service';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,58 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  
+  now: number;
+  pincode : number;
+  slotFinderStatus: boolean = true;
+  setIntervalForApiCall;
+  vaccineSlotsArray: Array<object> = [];
+  sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  constructor(private dataService: DataService) {
+    this.slotFinderStatusChange("wcv");
+    setInterval(() => {
+      this.now = Date.now();
+    }, 1);
+  }
+
+  slotFinderStatusChange(event) {
+    if (this.slotFinderStatus) {
+      this.setIntervalForApiCall = setInterval(() => {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        var liveDate = dd+'-'+mm+'-'+yyyy;
+
+        
+        var apiparam = {
+          date: liveDate,
+          pincode : this.pincode
+        }
+        this.dataService.sendGetRequest(apiparam).subscribe((data: any[]) => {
+          data['centers'][0].time = this.now;
+          this.vaccineSlotsArray.push(data)
+          console.log(data);
+
+          if (data['centers'][0].sessions[0].available_capacity > 0 || data['centers'][0].sessions[0].available_capacity > 0) {
+            var audio = new Audio();
+            audio.src = "../../../assets/beep.mp3";
+            audio.load();
+            setTimeout(() => {
+              audio.play();
+              setTimeout(() => {
+                audio.play();
+                setTimeout(() => {
+                  audio.play();
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }
+        })
+      }, 5000)
+    } else {
+      clearInterval(this.setIntervalForApiCall)
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataService } from './app.service';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers : [DataService]
 })
 export class AppModule { }
